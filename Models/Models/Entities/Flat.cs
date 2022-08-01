46d8c061d8e594fbe32e4c2c122fc5531f701cfd
betulbusradinler <busra.betul67@gmail.com>
2022-08-01 17:54:45 +0300

    Flat model class created

---
+++ Models/Models/Entities/Flat.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.Entities
+{
+    public class Flat
+    {
+        // Hangi blokda - Durumu (Dolu-boş) - Tipi (2+1 vb.) - Bulunduğu kat - Daire numarası - Daire sahibi veya kiracı
+
+        public int Id { get; set; }
+        public string No { get; set; }       
+        public int OwnerShipFlatId { get; set; }
+        public string Floor { get; set; }
+        public char Block { get; set; }
+        public string Type { get; set; }
+        public bool State { get; set; }
+
+        [ForeignKey("OwnerShipFlatId")]
+        public User OwnershipFlatOrHirer { get; set; }
+    }
+}
